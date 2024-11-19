9fb01462af88625257c64a8f48727fcdec123196
TheHighWhale <chris.harrhy@hotmail.co.uk>
2024-11-19 18:06:55 +0000

    Update

--- Assets/Scripts/UI/Menus/PauseMenu.cs
+++ Assets/Scripts/UI/Menus/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -38,5 +39,26 @@ public class PauseMenu : MonoBehaviour
         {
             return;
         }
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void ShowSettings()
+    {
+
+    }
+
+    public void Resume()
+    {
+        pauseCanvas.enabled = false;
+        paused = false;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
